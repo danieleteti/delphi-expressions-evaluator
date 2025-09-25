9aa657f8729ffb965531e2eed7cf00caff678a97
Daniele Teti <d.teti@bittime.it>
2025-09-25 13:13:47 +0200

    First Commit

---
+++ ExprEvaluator.pas
@@ -0,0 +1,575 @@
+﻿unit ExprEvaluator;
+
+interface
+
+uses
+  System.SysUtils, System.Variants, System.Math, System.Generics.Collections;
+
+type
+  TFuncHandler = reference to function(const Args: array of Variant): Variant;
+
+  TExprEvaluator = class
+  private
+    FFunctions: TDictionary<string, TFuncHandler>;
+    FVariables: TDictionary<string, Variant>;
+    FInput: string;
+    FPos: Integer;
+
+    function ParseExpression: Variant;
+    function ParseIfExpression: Variant;
+    function ParseLogical: Variant;
+    function ParseRelational: Variant;
+    function ParseAdditive: Variant;
+    function ParseMultiplicative: Variant;
+    function ParseFactor: Variant;
+    function ParsePrimary: Variant;
+    function ParseString: string;
+    function CurrentChar: Char;
+    procedure NextChar;
+    procedure SkipWhitespace;
+    function IsDigit(c: Char): Boolean;
+    function IsAlpha(c: Char): Boolean;
+    function ParseNumber: string;
+    function ParseIdentifier: string;
+    function CallFunction(const FuncName: string; Args: TArray<Variant>): Variant;
+    function GetVariable(const VarName: string): Variant;
+    procedure SetVariable(const VarName: string; Value: Variant);
+    function ParseAssignment: Variant;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure RegisterFunction(const Name: string; Handler: TFuncHandler);
+    function Evaluate(const Expr: string): Variant;
+    procedure SetVar(const Name: string; Value: Variant);
+    function GetVar(const Name: string): Variant;
+  end;
+
+implementation
+
+{ TExprEvaluator }
+
+constructor TExprEvaluator.Create;
+begin
+  inherited;
+  FFunctions := TDictionary<string, TFuncHandler>.Create;
+  FVariables := TDictionary<string, Variant>.Create;
+
+  // Register standard functions
+  RegisterFunction('sqrt', function(const Args: array of Variant): Variant
+    begin
+      if Length(Args) <> 1 then
+        raise Exception.Create('sqrt requires 1 argument');
+      Result := Sqrt(Args[0]);
+    end);
+
+  RegisterFunction('logn', function(const Args: array of Variant): Variant
+    begin
+      if Length(Args) <> 1 then
+        raise Exception.Create('logn requires 1 argument');
+      Result := Ln(Args[0]);
+    end);
+
+  RegisterFunction('log', function(const Args: array of Variant): Variant
+    begin
+      if Length(Args) <> 1 then
+        raise Exception.Create('log requires 1 argument');
+      Result := System.Math.Log10(Args[0]);
+    end);
+
+  RegisterFunction('ToString', function(const Args: array of Variant): Variant
+    begin
+      if Length(Args) <> 1 then
+        raise Exception.Create('ToString requires 1 argument');
+      Result := VarToStr(Args[0]);
+    end);
+end;
+
+destructor TExprEvaluator.Destroy;
+begin
+  FFunctions.Free;
+  FVariables.Free;
+  inherited;
+end;
+
+procedure TExprEvaluator.RegisterFunction(const Name: string; Handler: TFuncHandler);
+begin
+  FFunctions.AddOrSetValue(UpperCase(Name), Handler);
+end;
+
+function TExprEvaluator.Evaluate(const Expr: string): Variant;
+var
+  Exprs: TArray<string>;
+  I: Integer;
+  OldInput: string;
+  OldPos: Integer;
+begin
+  // Salviamo lo stato attuale
+  OldInput := FInput;
+  OldPos := FPos;
+
+  // Split by semicolon to support multiple expressions
+  Exprs := Expr.Split([';']);
+  Result := Unassigned;
+  for I := 0 to High(Exprs) do
+  begin
+    FInput := Trim(Exprs[I]);
+    FPos := 1;
+    Result := ParseAssignment;
+  end;
+
+  // Ripristiniamo lo stato originale
+  FInput := OldInput;
+  FPos := OldPos;
+end;
+
+function TExprEvaluator.ParseAssignment: Variant;
+var
+  Id: string;
+  OldPos: Integer;
+begin
+  SkipWhitespace;
+  if IsAlpha(CurrentChar) then
+  begin
+    OldPos := FPos;
+    Id := ParseIdentifier;
+    SkipWhitespace;
+    if Copy(FInput, FPos, 2) = ':=' then
+    begin
+      Inc(FPos, 2); // skip :=
+      SkipWhitespace;
+      Result := ParseExpression;
+      SetVariable(Id, Result);
+    end
+    else
+    begin
+      FPos := OldPos; // proper backtrack
+      Result := ParseIfExpression;
+    end;
+  end
+  else
+    Result := ParseIfExpression;
+end;
+
+function TExprEvaluator.ParseIfExpression: Variant;
+var
+  Condition, ThenValue, ElseValue: Variant;
+  IfWord: string;
+  OldPos: Integer;
+begin
+  SkipWhitespace;
+  if FPos > Length(FInput) then
+  begin
+    Result := ParseLogical;
+    Exit;
+  end;
+
+  // Check if this looks like an IF statement
+  if not IsAlpha(CurrentChar) then
+  begin
+    Result := ParseLogical;
+    Exit;
+  end;
+
+  OldPos := FPos;
+  IfWord := ParseIdentifier;
+  if IfWord <> 'IF' then
+  begin
+    FPos := OldPos; // proper backtrack
+    Result := ParseLogical;
+    Exit;
+  end;
+
+  Condition := ParseLogical;
+
+  SkipWhitespace;
+  if ParseIdentifier <> 'THEN' then
+    raise Exception.Create('Expected THEN after IF condition');
+
+  ThenValue := ParseIfExpression;
+
+  SkipWhitespace;
+  if ParseIdentifier <> 'ELSE' then
+    raise Exception.Create('Expected ELSE after THEN');
+
+  ElseValue := ParseIfExpression;
+
+  if Condition then
+    Result := ThenValue
+  else
+    Result := ElseValue;
+end;
+
+function TExprEvaluator.CurrentChar: Char;
+begin
+  if FPos <= Length(FInput) then
+    Result := FInput[FPos]
+  else
+    Result := #0;
+end;
+
+procedure TExprEvaluator.NextChar;
+begin
+  Inc(FPos);
+end;
+
+procedure TExprEvaluator.SkipWhitespace;
+begin
+  while (FPos <= Length(FInput)) and (CharInSet(FInput[FPos], [' ', #9, #10, #13])) do
+    Inc(FPos);
+end;
+
+function TExprEvaluator.IsDigit(c: Char): Boolean;
+begin
+  Result := CharInSet(c, ['0'..'9']);
+end;
+
+function TExprEvaluator.IsAlpha(c: Char): Boolean;
+begin
+  Result := CharInSet(c, ['a'..'z', 'A'..'Z', '_']);
+end;
+
+function TExprEvaluator.ParseNumber: string;
+var
+  Start: Integer;
+begin
+  Start := FPos;
+  while (FPos <= Length(FInput)) and (IsDigit(CurrentChar) or (CurrentChar = '.')) do
+    NextChar;
+  Result := Copy(FInput, Start, FPos - Start);
+end;
+
+function TExprEvaluator.ParseIdentifier: string;
+var
+  Start: Integer;
+begin
+  Start := FPos;
+  while (FPos <= Length(FInput)) and (IsAlpha(CurrentChar) or IsDigit(CurrentChar)) do
+    NextChar;
+  Result := UpperCase(Copy(FInput, Start, FPos - Start));
+end;
+
+function TExprEvaluator.ParseString: string;
+var
+  Start: Integer;
+begin
+  NextChar; // skip opening "
+  Start := FPos;
+  while (FPos <= Length(FInput)) and (CurrentChar <> '"') do
+    NextChar;
+  if CurrentChar <> '"' then
+    raise Exception.Create('Unterminated string');
+  Result := Copy(FInput, Start, FPos - Start);
+  NextChar; // skip closing "
+end;
+
+function TExprEvaluator.ParsePrimary: Variant;
+var
+  NumStr: string;
+  Id: string;
+  Args: TArray<Variant>;
+begin
+  SkipWhitespace;
+
+  if FPos > Length(FInput) then
+  begin
+    Result := Unassigned;
+    Exit;
+  end;
+
+  if CurrentChar = '(' then
+  begin
+    NextChar; // skip '('
+    Result := ParseExpression;
+    SkipWhitespace;
+    if CurrentChar <> ')' then
+      raise Exception.Create('Unclosed parenthesis');
+    NextChar; // skip ')'
+  end
+  else if CurrentChar = '"' then
+  begin
+    Result := ParseString;
+  end
+  else if IsDigit(CurrentChar) then
+  begin
+    NumStr := ParseNumber;
+    Result := StrToFloat(NumStr, TFormatSettings.Create('en-US'));
+  end
+  else if IsAlpha(CurrentChar) then
+  begin
+    Id := ParseIdentifier;
+    SkipWhitespace;
+    if CurrentChar = '(' then
+    begin
+      NextChar; // skip '('
+      SetLength(Args, 0);
+      if CurrentChar <> ')' then
+      begin
+        repeat
+          SetLength(Args, Length(Args) + 1);
+          Args[High(Args)] := ParseExpression;
+          SkipWhitespace;
+          if CurrentChar = ',' then
+          begin
+            NextChar;
+            SkipWhitespace;
+          end
+          else
+            Break;
+        until False;
+      end;
+      if CurrentChar <> ')' then
+        raise Exception.Create('Unclosed parenthesis in function call');
+      NextChar; // skip ')'
+      Result := CallFunction(Id, Args);
+    end
+    else
+    begin
+      Result := GetVariable(Id);
+    end;
+  end
+  else
+    raise Exception.Create('Unexpected character: ' + QuotedStr(CurrentChar));
+end;
+
+function TExprEvaluator.ParseFactor: Variant;
+var
+  Left: Variant;
+  Right: Variant;
+begin
+  Left := ParsePrimary;
+  if VarIsEmpty(Left) or VarIsNull(Left) then
+    raise Exception.Create('Expected expression');
+
+  SkipWhitespace;
+  while CurrentChar = '^' do
+  begin
+    NextChar; // skip ^
+    Right := ParsePrimary;
+    if VarIsEmpty(Right) or VarIsNull(Right) then
+      raise Exception.Create('Expected expression after ^');
+
+    Left := System.Math.Power(Left, Right);
+    SkipWhitespace;
+  end;
+  Result := Left;
+end;
+
+function TExprEvaluator.ParseMultiplicative: Variant;
+var
+  Left: Variant;
+  Op: Char;
+  Right: Variant;
+begin
+  Left := ParseFactor;
+  SkipWhitespace;
+
+  while CharInSet(CurrentChar, ['*', '/', 'm', 'M']) do
+  begin
+    if CharInSet(CurrentChar, ['m', 'M']) then
+    begin
+      if (UpperCase(Copy(FInput, FPos, 3)) = 'MOD') and
+         ((FPos + 3 > Length(FInput)) or not (IsAlpha(FInput[FPos + 3]) or IsDigit(FInput[FPos + 3]))) then
+      begin
+        Inc(FPos, 3);
+        SkipWhitespace;
+        Right := ParseFactor;
+        Left := Trunc(Left) mod Trunc(Right);
+      end
+      else
+        Break;
+    end
+    else
+    begin
+      Op := CurrentChar;
+      NextChar;
+      Right := ParseFactor;
+
+      case Op of
+        '*': Left := Left * Right;
+        '/': Left := Left / Right;
+      end;
+    end;
+    SkipWhitespace;
+  end;
+
+  Result := Left;
+end;
+
+function TExprEvaluator.ParseAdditive: Variant;
+var
+  Left: Variant;
+  Op: Char;
+  Right: Variant;
+begin
+  Left := ParseMultiplicative;
+  SkipWhitespace;
+
+  while CharInSet(CurrentChar, ['+', '-']) do
+  begin
+    Op := CurrentChar;
+    NextChar;
+    Right := ParseMultiplicative;
+
+    case Op of
+      '+':
+        begin
+          if VarIsStr(Left) or VarIsStr(Right) then
+            Left := VarToStr(Left) + VarToStr(Right)
+          else
+            Left := Left + Right;
+        end;
+      '-': Left := Left - Right;
+    end;
+    SkipWhitespace;
+  end;
+
+  Result := Left;
+end;
+
+function TExprEvaluator.ParseRelational: Variant;
+var
+  Left: Variant;
+  Right: Variant;
+begin
+  Left := ParseAdditive;
+  SkipWhitespace;
+
+  if CharInSet(CurrentChar, ['<', '>', '=']) then
+  begin
+    if CurrentChar = '=' then
+    begin
+      NextChar;
+      Right := ParseAdditive;
+      Result := Left = Right;
+    end
+    else if CurrentChar = '<' then
+    begin
+      NextChar;
+      if CurrentChar = '>' then
+      begin
+        NextChar;
+        Right := ParseAdditive;
+        Result := Left <> Right;
+      end
+      else if CurrentChar = '=' then
+      begin
+        NextChar;
+        Right := ParseAdditive;
+        Result := Left <= Right;
+      end
+      else
+      begin
+        Right := ParseAdditive;
+        Result := Left < Right;
+      end;
+    end
+    else if CurrentChar = '>' then
+    begin
+      NextChar;
+      if CurrentChar = '=' then
+      begin
+        NextChar;
+        Right := ParseAdditive;
+        Result := Left >= Right;
+      end
+      else
+      begin
+        Right := ParseAdditive;
+        Result := Left > Right;
+      end;
+    end;
+  end
+  else
+    Result := Left;
+end;
+
+function TExprEvaluator.ParseExpression: Variant;
+begin
+  Result := ParseIfExpression;
+end;
+
+function TExprEvaluator.ParseLogical: Variant;
+var
+  Left: Variant;
+  Right: Variant;
+begin
+  Left := ParseRelational;
+  SkipWhitespace;
+
+  while True do
+  begin
+    if (UpperCase(Copy(FInput, FPos, 3)) = 'AND') and 
+       ((FPos + 3 > Length(FInput)) or not (IsAlpha(FInput[FPos + 3]) or IsDigit(FInput[FPos + 3]))) then
+    begin
+      Inc(FPos, 3);
+      SkipWhitespace;
+      Right := ParseRelational;
+      Left := Left and Right;
+      SkipWhitespace;
+    end
+    else if (UpperCase(Copy(FInput, FPos, 2)) = 'OR') and
+            ((FPos + 2 > Length(FInput)) or not (IsAlpha(FInput[FPos + 2]) or IsDigit(FInput[FPos + 2]))) then
+    begin
+      Inc(FPos, 2);
+      SkipWhitespace;
+      Right := ParseRelational;
+      Left := Left or Right;
+      SkipWhitespace;
+    end
+    else if (UpperCase(Copy(FInput, FPos, 3)) = 'XOR') and
+            ((FPos + 3 > Length(FInput)) or not (IsAlpha(FInput[FPos + 3]) or IsDigit(FInput[FPos + 3]))) then
+    begin
+      Inc(FPos, 3);
+      SkipWhitespace;
+      Right := ParseRelational;
+      Left := Left xor Right;
+      SkipWhitespace;
+    end
+    else
+      Break;
+  end;
+
+  Result := Left;
+end;
+
+function TExprEvaluator.CallFunction(const FuncName: string; Args: TArray<Variant>): Variant;
+var
+  Handler: TFuncHandler;
+begin
+  if not FFunctions.TryGetValue(FuncName, Handler) then
+    raise Exception.Create('Unknown function: ' + FuncName);
+  Result := Handler(Args);
+end;
+
+function TExprEvaluator.GetVariable(const VarName: string): Variant;
+var
+  UpName: string;
+begin
+  UpName := UpperCase(VarName);
+  if UpName = 'TRUE' then
+    Result := True
+  else if UpName = 'FALSE' then
+    Result := False
+  else if (UpName = 'IF') or (UpName = 'THEN') or (UpName = 'ELSE') or 
+          (UpName = 'AND') or (UpName = 'OR') or (UpName = 'XOR') or (UpName = 'MOD') then
+    raise Exception.Create('Unexpected use of keyword: ' + VarName)
+  else if not FVariables.TryGetValue(UpName, Result) then
+    raise Exception.Create('Unknown variable: ' + VarName);
+end;
+
+procedure TExprEvaluator.SetVariable(const VarName: string; Value: Variant);
+begin
+  FVariables.AddOrSetValue(UpperCase(VarName), Value);
+end;
+
+procedure TExprEvaluator.SetVar(const Name: string; Value: Variant);
+begin
+  SetVariable(Name, Value);
+end;
+
+function TExprEvaluator.GetVar(const Name: string): Variant;
+begin
+  Result := GetVariable(Name);
+end;
+
+end.
---
+++ TestEvaluator.dpr
@@ -0,0 +1,170 @@
+﻿program TestEvaluator;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  System.Math,
+  System.Variants,
+  ExprEvaluator;
+
+procedure TestBasicArithmetic;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('3 + 2') = 5, '3 + 2 should be 5');
+    Assert(Eval.Evaluate('3 * (3 + 2)') = 15, '3 * (3 + 2) should be 15');
+    Assert(Eval.Evaluate('3 ^ 2') = 9, '3 ^ 2 should be 9');
+    Assert(Eval.Evaluate('10 mod 3') = 1, '10 mod 3 should be 1');
+    Writeln('Basic arithmetic tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestStringConcatenation;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('"Hello" + "World"') = 'HelloWorld', '"Hello" + "World" should be HelloWorld');
+    Assert(Eval.Evaluate('"Delphi" + " " + "Rules"') = 'Delphi Rules', '"Delphi" + " " + "Rules" should be "Delphi Rules"');
+    Writeln('String concatenation tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestVariables;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Eval.Evaluate('x := 10');
+    Eval.Evaluate('y := 5');
+    Assert(Eval.Evaluate('x + y') = 15, 'x + y should be 15');
+    Assert(Eval.Evaluate('x * y') = 50, 'x * y should be 50');
+    Writeln('Variable tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestRelationalOperators;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('5 > 3') = True, '5 > 3 should be True');
+    Assert(Eval.Evaluate('x := 10; y := 20; x < y') = True, 'x < y should be True');
+    Assert(Eval.Evaluate('x = y') = False, 'x = y should be False');
+    Assert(Eval.Evaluate('x <> y') = True, 'x <> y should be True');
+    Writeln('Relational operators tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestLogicalOperators;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('True and False') = False, 'True and False should be False');
+    Assert(Eval.Evaluate('True or False') = True, 'True or False should be True');
+    Assert(Eval.Evaluate('True xor False') = True, 'True xor False should be True');
+    Writeln('Logical operators tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestFunctions;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('sqrt(16)') = 4, 'sqrt(16) should be 4');
+    Assert(Abs(Eval.Evaluate('logn(2.71828)') - 1) < 0.01, 'logn(2.71828) should be ~1');
+    Assert(Eval.Evaluate('log(100)') = 2, 'log(100) should be 2');
+    Assert(Eval.Evaluate('ToString(42)') = '42', 'ToString(42) should be "42"');
+    Writeln('Function tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestIfThenElse;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('if 3 > 2 then "Yes" else "No"') = 'Yes', 'if 3 > 2 then "Yes" else "No" should be "Yes"');
+    Assert(Eval.Evaluate('if 1 > 2 then "A" else if 2 > 1 then "B" else "C"') = 'B', 'Nested if should return "B"');
+    Writeln('If-Then-Else tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestCustomFunction;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Eval.RegisterFunction('cube', function(const Args: array of Variant): Variant
+      begin
+        if Length(Args) <> 1 then
+          raise Exception.Create('cube requires 1 argument');
+        Result := System.Math.Power(Args[0], 3);
+      end);
+
+    Assert(Eval.Evaluate('cube(3)') = 27, 'cube(3) should be 27');
+    Writeln('Custom function tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+procedure TestMultipleExpressions;
+var
+  Eval: TExprEvaluator;
+begin
+  Eval := TExprEvaluator.Create;
+  try
+    Assert(Eval.Evaluate('a := 5; b := 10; a + b') = 15, 'a := 5; b := 10; a + b should be 15');
+    Assert(Eval.Evaluate('x := if 10 > 5 then 1 else 0; x') = 1, 'x := if 10 > 5 then 1 else 0; x should be 1');
+    Writeln('Multiple expressions tests passed.');
+  finally
+    Eval.Free;
+  end;
+end;
+
+begin
+  try
+    TestBasicArithmetic;
+    TestStringConcatenation;
+    TestVariables;
+    TestRelationalOperators;
+    TestLogicalOperators;
+    TestFunctions;
+    TestIfThenElse;
+    TestCustomFunction;
+    TestMultipleExpressions;
+
+    Writeln('All tests passed!');
+  except
+    on E: Exception do
+      Writeln('Test failed: ', E.Message);
+  end;
+  Readln;
+end.
